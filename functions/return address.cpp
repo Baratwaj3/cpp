fc0ac6d4deaf16c8a3e0987dd11897d6e235b1b4
Baratwaj3 <baratwaj3@gmail.com>
2025-04-27 21:12:07 +0530

    final changes done

--- functions/return address.cpp
+++ functions/return address.cpp
@@ -1,18 +1,22 @@
 #include <iostream>
 using namespace std;
-
-void returnByAddress(int *p) {
-    *p = 10; 
+int* fun (int size)
+{
+    int *p= new int [size];
+    for(int i=0;i<size;i++)
+    {
+        p[i]=i+1;
+       
+    }
+     return p;
 }
-
-int main() {
-    int num = 5;
-
-    cout << "Before function call: " << num << endl;
-
-    returnByAddress(&num);
-
-    cout << "After function call: " << num << endl;
-
+int main ()
+{
+    int * ptr= fun(5);
+    for (int i=0;i<5;i++)
+    {
+         cout<<ptr[i]<<endl;
+    }
+   
     return 0;
 }
