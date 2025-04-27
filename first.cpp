ea67ae5f83b20e245446c38cdff2cf0da2145d6a
Baratwaj3 <baratwaj3@gmail.com>
2025-04-27 20:20:10 +0530

    final

--- first.cpp
+++ first.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
 using namespace std;
-
-void returnByAddress(int *p) {
-    *p = 10; 
-
-int main() {
-    int num = 5;
-
-    cout << "Before function call: " << num << endl;
-
-    returnByAddress(&num);
-
-    cout << "After function call: " << num << endl;
-
+int main()
+{
+    cout<<"hello world ";
     return 0;
 }
