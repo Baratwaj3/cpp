a261d84507d9a714b6907422984921327f292ded
Baratwaj3 <baratwaj3@gmail.com>
2025-04-28 16:51:59 +0530

    just added accessing global variable with scope resolution (: :)

--- functions/local and global variable .cpp
+++ functions/local and global variable .cpp
@@ -4,33 +4,34 @@ using namespace std;
 int g = 10;  // Global variable
 
 void scope() {
-    g++; //
+    
     int a = 5;
     int l = g + a;  //  uses global 'g'
-    cout<<"Adding global variable with a :"<<l<<endl;
+    cout<<"Adding global variable with a : "<<l<<endl;
     
     cout << "accessing the global variable anywhere in the code: "<< g << endl;
 
     {
         int g = 20;  // New local 'g' inside this block
         g++;
+        cout<<"Getting gloable variable when it is hide by local variable in this block : "<<::g<<endl;
         cout << "Local to the block: ";
         cout << g << endl;
     }
 }
 
 int main() {
-    cout<<"Getting  global variable before changes:"<< g<<endl; // getting global variable 
+    cout<<"Getting  global variable before changes: "<< g<<endl; // getting global variable 
     scope();
-    cout<<"Getting  global variable after changes:"<< g<<endl;// getting after the function call
+    cout<<"Getting  global variable after changes: "<< g<<endl;// getting after the function call
     return 0;
 }
 
 // output :
-//    Getting  global variable before changes:10
-//    Adding global variable with a :16
-//    accessing the global variable anywhere in the code: 11
-//    Local to the block: 21
-//    Getting  global variable after changes:11
-
+//   Getting  global variable before changes: 10
+//   Adding global variable with a : 15
+//   accessing the global variable anywhere in the code: 10
+//   Getting gloable variable when it is hide by local variable in   this block : 10
+//   Local to the block: 21
+//   Getting  global variable after changes: 10
 
