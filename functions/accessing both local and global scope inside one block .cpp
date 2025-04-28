dc8cfd0b1791aaeba17826b46a93949e66ac92d5
Baratwaj3 <baratwaj3@gmail.com>
2025-04-28 17:54:06 +0530

    final with output

---
+++ functions/accessing both local and global scope inside one block .cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int g = 10;  // Global variable g
+
+void scope()
+
+    {
+        int g = 20; 
+        g++; // local g 
+       cout<<"incrementing local variable : "<<g<<endl;
+       
+       ::g++; // using scope resulation for accessing global var
+       cout<<"incrementing global variable : "<<::g<<endl;
+    }
+int main() {
+    
+    scope();
+    return 0;
+}
+//output:
+//    incrementing local variable : 21
+//    incrementing global variable : 11
