bffd93ff52684a71e1d4ec95b2538ae7a63fc3fb
Baratwaj3 <baratwaj3@gmail.com>
2025-04-29 17:27:00 +0530

    linear search using function

---
+++ functions/7.exercise-linear search using function.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+ int search (int A[] , int num , int k)
+ {
+     for (int i =0; i<=num ; i++)
+     {
+        if( k==A[i])
+         return i ;
+     }
+    return 0;
+ }
+ int main()
+ {
+     int num;
+     
+     cout<<"Enter the number of elements"<<endl;
+     cin>>num;
+     
+     int A[num] ;
+     
+     cout<<"Enter"<<" "<<num<<" "<<"to search"<<endl;
+     for (int i=0; i<num; i++)
+     {
+         cin >>A[i];
+     }
+     
+     int k;
+     cout<<"Enter the number to be searched :"<<endl;
+     cin>>k;
+     
+     int index = search (A,6,k);
+     
+     if (index!=0)
+     cout<<"The number is found at the index :"<< index<<endl;
+     else
+     cout<<"The number is not found"<<endl;
+     
+     return 0;
+     }
+     
+//   output 1:
+//     Enter the number of elements
+//     3
+//     Enter 3 to search
+//     1
+//     2
+//     3
+//     Enter the number to be searched :
+//     3
+//     The number is found at the index :2
+
+//   output 2:
+//     Enter the number of elements
+//     3
+//     Enter 3 to search
+//     3
+//     2
+//     1
+//     Enter the number to be searched :
+//     9
+//     The number is not found
+
+
+
+    
+ 
