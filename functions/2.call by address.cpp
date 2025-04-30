f65224e4c845b92243ef5a7c0343ff224c0732d4
Baratwaj3 <baratwaj3@gmail.com>
2025-04-30 23:34:49 +0530

    call by address with output

---
+++ functions/2.call by address.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void swap(int *a, int *b)
+{
+    int temp = *b;
+    *b = *a;
+    *a = temp;
+}
+int main()
+{
+
+    int a, b;
+    a = 5;
+    b = 6;
+    swap(&a, &b);
+    cout << a << "\n"
+         << b << endl;
+    return 0;
+}
+// output :
+//    6
+//    5
+
+// Here the fuenction does not return anything it just modifies the value by using pointers in the memory
