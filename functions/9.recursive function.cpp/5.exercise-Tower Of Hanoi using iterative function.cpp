21f8343ca9bfea8b79b28abaf5459d10e0931c19
Baratwaj3 <baratwaj3@gmail.com>
2025-04-30 23:57:03 +0530

    ffinal with output

--- functions/9.recursive function.cpp/5.exercise-Tower Of Hanoi using iterative function.cpp
+++ functions/9.recursive function.cpp/5.exercise-Tower Of Hanoi using iterative function.cpp
@@ -58,3 +58,11 @@ int main() {
     towerOfHanoiIterative(n, 'A', 'C', 'B');
     return 0;
 }
+//Enter number of disks: 3
+//    Move disk 1 from A to C
+//    Move disk 2 from A to B
+//    Move disk 1 from C to B
+//    Move disk 3 from A to C
+//    Move disk 1 from B to A
+//    Move disk 2 from B to C
+//    Move disk 1 from A to C
