f5e21eb8eb9b656e458a8a9bf765b79dac42c436
Baratwaj3 <baratwaj3@gmail.com>
2025-05-01 23:32:13 +0530

    argument function

---
+++ functions/12.Default Argument.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int add(int x, int y = 0, int z = 0)
+{
+    return x + y + z;
+}
+int main()
+{
+    int a = add(2);
+    int b = add(2, 3);
+    int c = add(2, 3, 4);
+    cout << a << "\n"<< b << "\n"<< c << endl;
+    return 0;
+}
