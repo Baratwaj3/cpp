26e6ade5b9ed66ff29c8b2fca7949a6e4ce5ce0c
Baratwaj3 <baratwaj3@gmail.com>
2025-05-01 23:42:20 +0530

    ordered and output added

--- functions/13.Function Overloading.cpp
+++ functions/13.Function Overloading.cpp
@@ -1,28 +1,33 @@
 #include <iostream>
 using namespace std;
-int add (int x, int y)
+int add(int x, int y)
 {
-    return x+y;
+    return x + y;
 }
-int add (int x, int y, int z)
+int add(int x, int y, int z)
 {
-    return x+y+z;
+    return x + y + z;
 }
-float add (float x, float y)
+float add(float x, float y)
 {
-    return x+y;
+    return x + y;
 }
-float add (float x, float y, float z)
+float add(float x, float y, float z)
 {
-    return x+y+z;
+    return x + y + z;
 }
-int main ()
+int main()
 {
-    int a=add(2,3);
-    int b=add(2,3,4);
-    float c=add(3.14f,1.73f);
-    float d=add(3.14f,1.73f,6.132f);
-    cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<d<<endl;
+    int a = add(2, 3);
+    int b = add(2, 3, 4);
+    float c = add(3.14f, 1.73f);
+    float d = add(3.14f, 1.73f, 6.132f);
+    cout << a << "\n"<< b << "\n"<< c << "\n"<< d << endl;
     return 0;
 }
 
+// output:
+//    5
+//    9
+//    4.87
+//    11.002
