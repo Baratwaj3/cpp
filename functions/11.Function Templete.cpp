6b38de5e7a4d654b894a059ace3a95a66cd9def7
Baratwaj3 <baratwaj3@gmail.com>
2025-05-01 23:20:51 +0530

    function templete

---
+++ functions/11.Function Templete.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+template <class T>
+T add(T x, T y)
+{
+    return x + y;
+}
+int main()
+{
+    int a = add(3, 2);
+    float b = add(3.14f, 1.73f);
+    cout << a << "\n"<< b << endl;
+    return0;
+}
