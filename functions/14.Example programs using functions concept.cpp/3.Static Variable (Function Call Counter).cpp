63487e87bcc15f6f647efb0938fc8176665d0f60
Baratwaj3 <baratwaj3@gmail.com>
2025-05-02 23:04:34 +0530

    eg code

---
+++ functions/14.Example programs using functions concept.cpp/3.Static Variable (Function Call Counter).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void counter() {
+    static int count = 0;
+    count++;
+    cout << "Function called " << count << " times." << endl;
+}
+
+int main() {
+    counter();
+    counter();
+    counter();
+    return 0;
+}
+
+/*
+Output:
+Function called 1 times.
+Function called 2 times.
+Function called 3 times.
+*/
