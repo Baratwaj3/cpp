96a595862e0f11849883323f0edc9dbc70d113f0
Baratwaj3 <baratwaj3@gmail.com>
2025-05-02 23:04:16 +0530

    eg code

---
+++ functions/14.Example programs using functions concept.cpp/1. Climbing Stairs (Recursion + Static Variable).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int climbStairs(int n) {
+    if (n <= 2)
+        return n;
+    return climbStairs(n - 1) + climbStairs(n - 2);
+}
+
+int main() {
+    int steps = 5;
+    cout << "Ways to climb " << steps << " steps: " << climbStairs(steps) << endl;
+    return 0;
+}
+
+
+//Output:
+//   Ways to climb 5 steps: 8
+
