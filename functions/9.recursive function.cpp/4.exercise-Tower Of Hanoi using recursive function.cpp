4ce13e62a95a0e0f8a47d91883894dfcfbecc1ed
Baratwaj3 <baratwaj3@gmail.com>
2025-05-05 18:04:38 +0530

    code changed

--- functions/9.recursive function.cpp/4.exercise-Tower Of Hanoi using recursive function.cpp
+++ functions/9.recursive function.cpp/4.exercise-Tower Of Hanoi using recursive function.cpp
@@ -1,33 +1,40 @@
 #include <iostream>
 using namespace std;
 
-void towerOfHanoi(int n, char from_rod, char to_rod, char aux_rod)
+void TOH(int n, char A, char B, char C)
 {
     if (n == 1)
     {
-        cout << "Move disk 1 from rod " << from_rod << " to rod " << to_rod << endl;
-        return;
+        cout<<"Move disk 1 from rod "<<A<<" to "<<C<<endl;
     }
-
-    towerOfHanoi(n - 1, from_rod, aux_rod, to_rod);
-
-    cout << "Move disk " << n << " from rod " << from_rod << " to rod " << to_rod << endl;
-
-    towerOfHanoi(n - 1, aux_rod, to_rod, from_rod);
+    else
+    {
+        TOH(n - 1, A, C, B);
+        cout<<"Move disk "<<n<<" from rod "<<A<<" to "<<C<<endl;
+        TOH(n - 1, B, A, C);
+    }
 }
 
 int main()
 {
-    int n = 3;
-    towerOfHanoi(n, 'A', 'C', 'B');
+    int n;
+    cout<<"Enter the number of disk: "<<endl;
+    cin>>n;
+    char Source = 'A';
+    char Auxiliary = 'B';
+    char Destination = 'C';
+
+    TOH(n, Source, Auxiliary,Destination );
     return 0;
 }
-
-// output :
-//   Move disk 1 from rod A to rod C
-//   Move disk 2 from rod A to rod B
-//   Move disk 1 from rod C to rod B
-//   Move disk 3 from rod A to rod C
-//   Move disk 1 from rod B to rod A
-//   Move disk 2 from rod B to rod C
-//   Move disk 1 from rod A to rod C
+/*
+output:
+     Enter the number of disk: 3
+     Move disk 1 from rod A to C
+     Move disk 2 from rod A to B
+     Move disk 1 from rod C to B
+     Move disk 3 from rod A to C
+     Move disk 1 from rod B to A
+     Move disk 2 from rod B to C 
+     Move disk 1 from rod A to C
+*/
