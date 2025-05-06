381609b0188707ed442a8f717d9f641802412e02
Baratwaj3 <baratwaj3@gmail.com>
2025-05-06 18:00:42 +0530

    string using class with output

---
+++ String/1.Declearing a string using class.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s[6] = {"hello", "world", "welcome", "to", "cpp"};
+    for (int i = 0; i < 6; i++)
+        cout << s[i] << endl;
+
+    return 0;
+}
+
+/*
+output:
+hello
+world
+welcome
+to
+cpp
+/*
