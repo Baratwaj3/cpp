e5a51e182f7219d158817c6cfbc053d04553c559
Baratwaj3 <baratwaj3@gmail.com>
2025-05-07 22:14:13 +0530

    length of the string

---
+++ String/Example programs using string.cpp/2.Finding Length of a string.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "Electrical";
+    cout << "Length of string: " << str.length() << endl;
+    return 0;
+}
+/*
+Length of string: 10
+
+*/
