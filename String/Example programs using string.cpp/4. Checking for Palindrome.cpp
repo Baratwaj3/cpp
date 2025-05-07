90bb808da0c53c1dfdc7e19dceae38e8fc97e650
Baratwaj3 <baratwaj3@gmail.com>
2025-05-07 22:17:07 +0530

    Checking for Palindrome

---
+++ String/Example programs using string.cpp/4. Checking for Palindrome.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str = "madam";
+    string rev = string(str.rbegin(), str.rend());
+    
+    if(str == rev)
+        cout << str << " is a palindrome." << endl;
+    else
+        cout << str << " is not a palindrome." << endl;
+    return 0;
+}
+/*
+madam is a palindrome.
+
+*/
