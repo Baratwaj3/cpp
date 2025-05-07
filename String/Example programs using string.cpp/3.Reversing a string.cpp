dc16bc1da29ef40865f785adef23c1319da4affe
Baratwaj3 <baratwaj3@gmail.com>
2025-05-07 22:16:00 +0530

    Reversing a String

---
+++ String/Example programs using string.cpp/3.Reversing a string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string str = "Engineering";
+    reverse(str.begin(), str.end());
+    cout << "Reversed string: " << str << endl;
+    return 0;
+}
+/*
+Reversed string: gnireenignE
+
+*/
