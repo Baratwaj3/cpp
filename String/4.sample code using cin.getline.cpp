91ed900501f9f40bebb7caae22bf92509ac352df
Baratwaj3 <baratwaj3@gmail.com>
2025-05-08 17:23:06 +0530

    using cin.getline

---
+++ String/4.sample code using cin.getline.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char s[50];
+    char s2[50];
+    cout << "Enter your name " << endl;
+    cin.getline(s, 50);
+    cout << "Welcome " << s << endl;
+
+    cout << "Enter your name again " << endl;
+    cin.getline(s2, 50);
+    cout << "Welcome " << s2 << endl;
+    return 0;
+}
+/*
+Enter your name
+Baratwaj
+Welcome Baratwaj
+Enter your name again
+Chandru
+Welcome Chandru
+
+*/
