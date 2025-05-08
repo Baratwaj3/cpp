8035c5a5a26b2125cef92f9df2c466635b892641
Baratwaj3 <baratwaj3@gmail.com>
2025-05-08 17:18:01 +0530

    using cin.get

---
+++ String/3.sample code using cin.get.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main() {
+   char s[50];
+   cout<<"Enter your name "<<endl;
+   cin.get(s,50);
+   cout<<"Welcome "<<s<<endl;
+    return 0;
+}
+/*
+Enter your name 
+Baratwaj
+Welcome Baratwaj
+*/
