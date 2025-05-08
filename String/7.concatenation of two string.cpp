dc0acf55e8f21596f49e64d98784bc66e731e978
Baratwaj3 <baratwaj3@gmail.com>
2025-05-08 17:40:57 +0530

    using cancadenation

---
+++ String/7.concatenation of two string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    char s[] = "hello";
+    char s2[] = "welcome";
+    strcat(s, s2);
+    cout << "Full concatenation: " << s << endl;
+
+    char s3[] = "hello";
+    strncat(s3, s2, 3);
+    cout << "Partial concatenation (3 chars): " << s3 << endl;
+    return 0;
+}
+/*
+Full concatenation: hellowelcome
+Partial concatenation (3 chars): hellowel
+*/
