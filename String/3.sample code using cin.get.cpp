e884cbe11ed2ce06ecf96dfaf422e1f0b65693fb
Baratwaj3 <baratwaj3@gmail.com>
2025-05-08 17:18:37 +0530

    indendation

--- String/3.sample code using cin.get.cpp
+++ String/3.sample code using cin.get.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
-int main() {
-   char s[50];
-   cout<<"Enter your name "<<endl;
-   cin.get(s,50);
-   cout<<"Welcome "<<s<<endl;
+int main()
+{
+    char s[50];
+    cout << "Enter your name " << endl;
+    cin.get(s, 50);
+    cout << "Welcome " << s << endl;
     return 0;
 }
 /*
-Enter your name 
+Enter your name
 Baratwaj
 Welcome Baratwaj
 */
