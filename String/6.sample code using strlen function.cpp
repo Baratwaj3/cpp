af54d4b2f5bbb784c7a872ed7b64c242f2033733
Baratwaj3 <baratwaj3@gmail.com>
2025-05-08 17:31:35 +0530

    using strlen

---
+++ String/6.sample code using strlen function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    char s[] = "hello";
+    int len = strlen(s);
+    cout << "The length of the string is " << len << endl;
+    return 0;
+}
+/*
+The length of the string is 5
+*/
