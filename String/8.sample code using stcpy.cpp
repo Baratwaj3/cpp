653946f323c054f0558c72a327a0d9f0ab470be6
Baratwaj3 <baratwaj3@gmail.com>
2025-05-08 17:55:35 +0530

    strcpy.cpp

---
+++ String/8.sample code using stcpy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main()
+{
+    char s1[] = "";
+    char s2[] = "welcome";
+    strcpy(s1, s2);
+    cout << "copying s2 in s1: " << s1 << endl;
+    return 0;
+}
+/*
+copying s2 in s1: welcome
+*/
