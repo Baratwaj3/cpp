31f859001459d716ae2c9965b2e1e26e1f041464
Baratwaj3 <baratwaj3@gmail.com>
2025-05-09 18:11:36 +0530

    small logic changed

--- String/10.Sample code using strchr.cpp
+++ String/10.Sample code using strchr.cpp
@@ -7,7 +7,7 @@ int main()
     cout << "Enter a string: ";
     cin >> s1;
     char s2;
-    cout << "Enter target string: ";
+    cout << "Enter target char: ";
     cin >> s2;
     char *x;          // search target from left to right
     x = strchr(s1, s2);
@@ -21,12 +21,12 @@ int main()
 /*
 output_1:
 Enter a string: programming
-Enter target string: r
+Enter target char: r
 First occuring of target word is : rogramming
 
 output_2:
 Enter a string: programming
-Enter target string: l
+Enter target char: l
 not found
 
 */
