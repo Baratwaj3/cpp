8ff46760e48558818e48c04a8b8c26e08983484d
Baratwaj3 <baratwaj3@gmail.com>
2025-05-09 17:42:02 +0530

    seperated for easy readability

--- String/9.sample code using strstr.cpp
+++ String/9.sample code using strstr.cpp
@@ -6,11 +6,14 @@ int main()
     char s1[50];
     cout << "Enter a string: ";
     cin >> s1;
+
     char s2[50];
     cout << "Enter target string: ";
     cin >> s2;
+
     if (strstr(s1, s2) != NULL)
         cout << strstr(s1, s2) << endl;
+        
     else
         cout << "not found";
     return 0;
