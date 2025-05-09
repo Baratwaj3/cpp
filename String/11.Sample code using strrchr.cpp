d198bdff580a5381b6bb0d8edf6574ac10a92a1f
Baratwaj3 <baratwaj3@gmail.com>
2025-05-09 18:12:20 +0530

    small logic changed

--- String/11.Sample code using strrchr.cpp
+++ String/11.Sample code using strrchr.cpp
@@ -7,7 +7,7 @@ int main()
     cout << "Enter a string: ";
     cin >> s1;
     char s2;
-    cout << "Enter target string: ";
+    cout << "Enter target char: ";
     cin >> s2;
     char *x;
     x = strrchr(s1, s2);        // it search from right to left 
@@ -21,7 +21,7 @@ int main()
 /*
 output_1:
 Enter a string: programming
-Enter target string: r
+Enter target char: r
 Last occuring of target word is : ramming
 
 output_2:
