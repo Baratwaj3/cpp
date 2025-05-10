2edd750aedd9b9cb249ce4a5ffa127eb636c1494
Baratwaj3 <baratwaj3@gmail.com>
2025-05-10 23:17:19 +0530

    alinged

--- functions/6.local and global variable.cpp
+++ functions/6.local and global variable.cpp
@@ -1,29 +1,31 @@
 #include <iostream>
 using namespace std;
 
-int g = 10;  // Global variable
+int g = 10; // Global variable
 
-void scope() {
-    
+void scope()
+{
+
     int a = 5;
-    int l = g + a;  //  uses global 'g'
-    cout<<"Adding global variable with a : "<<l<<endl;
-    
-    cout << "accessing the global variable anywhere in the code: "<< g << endl;
+    int l = g + a; //  uses global 'g'
+    cout << "Adding global variable with a : " << l << endl;
+
+    cout << "accessing the global variable anywhere in the code: " << g << endl;
 
     {
-        int g = 20;  // New local 'g' inside this block
+        int g = 20; // New local 'g' inside this block
         g++;
-        cout<<"Getting gloable variable when it is hide by local variable in this block : "<<::g<<endl;
+        cout << "Getting gloable variable when it is hide by local variable in this block : " << ::g << endl;
         cout << "Local to the block: ";
         cout << g << endl;
     }
 }
 
-int main() {
-    cout<<"Getting  global variable before changes: "<< g<<endl; // getting global variable 
+int main()
+{
+    cout << "Getting  global variable before changes: " << g << endl; // getting global variable
     scope();
-    cout<<"Getting  global variable after changes: "<< g<<endl;// getting after the function call
+    cout << "Getting  global variable after changes: " << g << endl; // getting after the function call
     return 0;
 }
 
@@ -35,5 +37,4 @@ output :
    Getting gloable variable when it is hide by local variable in   this block : 10
    Local to the block: 21
    Getting  global variable after changes: 10
-*/   
-
+*/
