9a7bae8baa8ba34160775bb4fd52830067dfd3e2
Baratwaj3 <baratwaj3@gmail.com>
2025-05-13 17:18:40 +0530

    using strtol

---
+++ String/14.Sample code using strtol.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include <cstring>
+int main() {
+    char A[]= "324";
+    int sample_test = strtol(A,NULL,10);
+    cout<<"Integer value : "<<sample_test<<endl;
+    sample_test+=1;
+    cout<<"Integer after added: "<<sample_test<<endl;
+     return 0;
+
+    }
+
+   
