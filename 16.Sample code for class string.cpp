cab565bee350ff14135d3bd5f6eedf944501ea77
Baratwaj3 <baratwaj3@gmail.com>
2025-05-14 18:44:45 +0530

    ...

--- 16.Sample code for class string.cpp
+++ 16.Sample code for class string.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
-int main() {
-   string str_1 = "HELLO WORLD";
-   cin>>str_1;
-   cout<<str_1<<endl;
-   
-   string str_2 = "HELLO WORLD";
-   getline (cin,str_2);
-   cout<<str_2<<endl;
+int main()
+{
+    string str_1 = "HELLO WORLD";
+    cin >> str_1;
+    cout << str_1 << endl;
+
+    string str_2 = "HELLO WORLD";
+    getline(cin, str_2);
+    cout << str_2 << endl;
 
     return 0;
 }
