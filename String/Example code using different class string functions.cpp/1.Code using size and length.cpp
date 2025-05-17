b062475c027f6886fe986d556f6f7bbfc409f189
Baratwaj3 <baratwaj3@gmail.com>
2025-05-17 21:27:54 +0530

    using size and length

---
+++ String/Example code using different class string functions.cpp/1.Code using size and length.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1 = "Hello";
+    cout << "Length: " << s1.length() << endl;
+
+    string s2 = "Git Hub";
+    cout << "Size: " << s2.size() << endl;
+
+    string s3 = " ";
+    cout << "Length of empty string: " << s3.length() << endl;
+}
