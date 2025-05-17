4601e072e6f364e7708cb98e00f577f3e67545ff
Baratwaj3 <baratwaj3@gmail.com>
2025-05-17 21:38:24 +0530

    using resize function

---
+++ String/Example code using different class string functions.cpp/3.Code using resize function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1 = "Hello";
+    s1.resize(3);
+    cout << "After resize to 3: " << s1 << endl;
+
+    string s2 = "World";
+    s2.resize(8, '*');
+    cout << "After resize to 8 with '*': " << s2 << endl;
+
+    string s3 = "Data";
+    s3.resize(10);
+    cout << "After resize to 10 (default char): " << s3 << endl;
+}
