5ba39fe45d7c4f1b4d087fc36f2ea3178723cf32
Baratwaj3 <baratwaj3@gmail.com>
2025-05-17 21:33:37 +0530

    using capacity

---
+++ String/Example code using different class string functions.cpp/2.Code using capacity function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1 = "Welcome";
+    cout << "Capacity: " << s1.capacity() << endl;
+
+    string s2 = "Artificial Intelligence";
+    cout << "Capacity: " << s2.capacity() << endl;
+
+    string s3 = "";
+    s3.reserve(50);
+    cout << "Reserved Capacity: " << s3.capacity() << endl;
+}
