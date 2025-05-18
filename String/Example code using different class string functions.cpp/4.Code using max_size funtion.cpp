709727160b04f3d372ebc8674439d1a1a9a6c27c
Baratwaj3 <baratwaj3@gmail.com>
2025-05-18 19:13:23 +0530

    using max_size

---
+++ String/Example code using different class string functions.cpp/4.Code using max_size funtion.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cout << "Max size: " << s.max_size() << endl;
+
+    string s1 = "abc";
+    cout << "Max size of small string: " << s1.max_size() << endl;
+
+    string s2 = "some large text here";
+    cout << "Max size: " << s2.max_size() << endl;
+}
