2caaaf3b23ed1cea685df2ee1689d9439537f3ef
Baratwaj3 <baratwaj3@gmail.com>
2025-05-18 19:26:11 +0530

    using empty function

---
+++ String/Example code using different class string functions.cpp/6.Code using empty function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s = "";
+    cout << "Is string empty? " << (s.empty() ? "Yes" : "No") << endl;
+
+    s = "Not empty now";
+    cout << "After assigning, is empty? " << (s.empty() ? "Yes" : "No") << endl;
+    return 0;
+}
