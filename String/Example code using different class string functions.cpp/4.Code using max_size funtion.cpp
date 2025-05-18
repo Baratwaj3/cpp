687ab571187617c8756cde6a7a0dd8f4dfcdbbbb
Baratwaj3 <baratwaj3@gmail.com>
2025-05-18 19:14:07 +0530

    output added

--- String/Example code using different class string functions.cpp/4.Code using max_size funtion.cpp
+++ String/Example code using different class string functions.cpp/4.Code using max_size funtion.cpp
@@ -13,3 +13,9 @@ int main()
     string s2 = "some large text here";
     cout << "Max size: " << s2.max_size() << endl;
 }
+/*
+output:
+Max size: 4611686018427387897
+Max size of small string: 4611686018427387897
+Max size: 4611686018427387897
+*/
