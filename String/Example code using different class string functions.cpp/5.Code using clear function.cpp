f4bd179e2073d00ae607bc88b77197790ee1079c
Baratwaj3 <baratwaj3@gmail.com>
2025-05-18 19:19:00 +0530

    using clear function

---
+++ String/Example code using different class string functions.cpp/5.Code using clear function.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s = "Clear this text";
+    cout << "Before clear: " << s << endl;
+    s.clear();
+    cout << "After clear: '" << s << "'" << endl;
+    return 0;
+}
