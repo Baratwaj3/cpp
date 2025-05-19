c45c5d2a3fec99b131d9ff0f4960af8a67b24b30
Baratwaj3 <baratwaj3@gmail.com>
2025-05-19 15:38:18 +0530

    using length function

---
+++ String/Example programs using string.cpp/Some other examples for string functions.cpp/1.Code using length function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string username = "user123";
+    cout << "Username: " << username << endl;
+    cout << "Length: " << username.length() << endl;
+    return 0;
+}
