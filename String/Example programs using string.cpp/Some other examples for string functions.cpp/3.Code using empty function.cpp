a129e18ee6e6b7befb4e6adc81e0509ac8b1a6e7
Baratwaj3 <baratwaj3@gmail.com>
2025-05-19 15:42:13 +0530

    using empty function

---
+++ String/Example programs using string.cpp/Some other examples for string functions.cpp/3.Code using empty function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string password = "";
+    if (password.empty()) {
+        cout << "Password field is empty!" << endl;
+    } else {
+        cout << "Password entered." << endl;
+    }
+    return 0;
+}
