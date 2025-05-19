18ad35093ae6c08e53ca6b9618e7627de1b24b96
Baratwaj3 <baratwaj3@gmail.com>
2025-05-19 15:38:35 +0530

    alinged

--- String/Example programs using string.cpp/Some other examples for string functions.cpp/1.Code using length function.cpp
+++ String/Example programs using string.cpp/Some other examples for string functions.cpp/1.Code using length function.cpp
@@ -2,7 +2,8 @@
 #include <string>
 using namespace std;
 
-int main() {
+int main()
+{
     string username = "user123";
     cout << "Username: " << username << endl;
     cout << "Length: " << username.length() << endl;
