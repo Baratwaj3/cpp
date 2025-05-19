52f41be6cb79e88f6d503033607e2e4d0a74f5ba
Baratwaj3 <baratwaj3@gmail.com>
2025-05-19 15:40:34 +0530

    using + operatot

---
+++ String/Example programs using string.cpp/Some other examples for string functions.cpp/2.Code using + Operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string first = "John";
+    string last = "Doe";
+    string fullName = first + " " + last;
+    cout << "Full Name: " << fullName << endl;
+    return 0;
+}
