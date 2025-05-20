61c5768dffa04751c40b408aa65126c1006fd708
Baratwaj3 <baratwaj3@gmail.com>
2025-05-20 22:04:12 +0530

    usinng find function

---
+++ String/Some other examples for class string functions.cpp/5.Code using find function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string email = "user@example.com";
+    if (email.find("@gmail.com") != string::npos)
+        cout << "Email is a Gmail address." << endl;
+    else
+        cout << "Email is not Gmail." << endl;
+    return 0;
+}
