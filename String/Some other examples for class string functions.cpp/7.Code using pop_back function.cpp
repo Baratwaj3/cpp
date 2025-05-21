cd49c05f1b2181166bc88b651bdbb6ac5679672c
Baratwaj3 <baratwaj3@gmail.com>
2025-05-21 22:39:44 +0530

    usng pop_back function

---
+++ String/Some other examples for class string functions.cpp/7.Code using pop_back function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string input = "Helloo";
+    input.pop_back();
+    cout << "Corrected input: " << input << endl;
+    return 0;
+}
