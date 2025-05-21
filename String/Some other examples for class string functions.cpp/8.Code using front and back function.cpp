f7dcb008ad14753d3c68e8efb0ff519be92af2ae
Baratwaj3 <baratwaj3@gmail.com>
2025-05-21 22:42:02 +0530

    using back anf front function

---
+++ String/Some other examples for class string functions.cpp/8.Code using front and back function.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string line = "CodeGPT";
+    cout << "First: " << line.front() << ", Last: " << line.back() << endl;
+    return 0;
+}
