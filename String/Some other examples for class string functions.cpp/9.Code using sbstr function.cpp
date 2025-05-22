6ebeaa94da858822acf90c0446a1a6ccabc1d88d
Baratwaj3 <baratwaj3@gmail.com>
2025-05-22 21:59:36 +0530

    using substr

---
+++ String/Some other examples for class string functions.cpp/9.Code using sbstr function.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string article = "This is a sample blog article.";
+    cout << "Preview: " << article.substr(0, 10) << "..." << endl;
+    return 0;
+}
