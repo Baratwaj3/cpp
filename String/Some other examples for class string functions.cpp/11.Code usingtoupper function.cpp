3dbddbc549526b5c066201ca1035db3f9b11598e
Baratwaj3 <baratwaj3@gmail.com>
2025-05-24 23:10:24 +0530

    code using toupper

---
+++ String/Some other examples for class string functions.cpp/11.Code usingtoupper function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name = "bob";
+    name[0] = toupper(name[0]);
+    cout << "Capitalized: " << name << endl;
+    return 0;
+}
