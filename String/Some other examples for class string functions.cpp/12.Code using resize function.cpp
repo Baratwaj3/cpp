dd29e4b68ced6fe952632ca12693d829bd1d6a5f
Baratwaj3 <baratwaj3@gmail.com>
2025-05-24 23:12:24 +0530

    using resize function

---
+++ String/Some other examples for class string functions.cpp/12.Code using resize function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string blog = "Welcome to the blog!";
+    blog.resize(10);
+    cout << "Preview: " << blog << "..." << endl;
+    return 0;
+}
