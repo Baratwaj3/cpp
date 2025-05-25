978e5e9b4c4c7216ebb7091ae9ec7958748c7046
Baratwaj3 <baratwaj3@gmail.com>
2025-05-25 22:54:24 +0530

    using find and replace

---
+++ String/Some other examples for class string functions.cpp/13.Code using find + repace function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string post = "I love Java!";
+    size_t pos = post.find("Java");
+    if (pos != string::npos)
+        post.replace(pos, 4, "C++");
+    cout << "Updated post: " << post << endl;
+    return 0;
+}
