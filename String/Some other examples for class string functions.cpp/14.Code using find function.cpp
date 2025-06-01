01aba8be0e3c4d938cb0aba8dca50684ed233170
Baratwaj3 <baratwaj3@gmail.com>
2025-06-01 21:39:59 +0530

    using find function

---
+++ String/Some other examples for class string functions.cpp/14.Code using find function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string comment = "This is a bad comment.";
+    if (comment.find("bad") != string::npos)
+        cout << "Inappropriate content found." << endl;
+    else
+        cout << "Comment is clean." << endl;
+    return 0;
+}
