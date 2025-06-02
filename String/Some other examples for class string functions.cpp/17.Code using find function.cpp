ffda1897cda61f3389621df5f4bd13d2030a491d
Baratwaj3 <baratwaj3@gmail.com>
2025-06-02 20:26:22 +0530

    find function

---
+++ String/Some other examples for class string functions.cpp/17.Code using find function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string url = "https://example.com";
+    if (url.find("https://") == 0)
+        cout << "Secure URL" << endl;
+    else
+        cout << "Not a secure URL" << endl;
+    return 0;
+}
