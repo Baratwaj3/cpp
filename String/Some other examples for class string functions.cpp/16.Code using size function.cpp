fef0f52f9f0c3eada62284cab4a3b717ffa06a1f
Baratwaj3 <baratwaj3@gmail.com>
2025-06-02 20:24:20 +0530

    size function

---
+++ String/Some other examples for class string functions.cpp/16.Code using size function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a = "apple";
+    string b = "banana";
+    if (a.size() > b.size())
+        cout << "String a is longer" << endl;
+    else
+        cout << "String b is longer" << endl;
+    return 0;
+}
