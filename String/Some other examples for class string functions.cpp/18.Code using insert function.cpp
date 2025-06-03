e348ce89178757aa499ac835a17f47a8a7e544c8
Baratwaj3 <baratwaj3@gmail.com>
2025-06-03 23:22:49 +0530

    output added

--- String/Some other examples for class string functions.cpp/18.Code using insert function.cpp
+++ String/Some other examples for class string functions.cpp/18.Code using insert function.cpp
@@ -4,8 +4,12 @@ using namespace std;
 
 int main()
 {
-    string name = "John Doe";
-    name.insert(5, "Michael ");
+    string name = "barat";
+    name.insert(5, "waj ");
     cout << "Full name: " << name << endl;
     return 0;
 }
+/*
+output:
+Full name: barat waj
+*/
