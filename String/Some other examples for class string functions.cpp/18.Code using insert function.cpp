a671c9a016ccb1f86fab06d51f33e755d55ddc43
Baratwaj3 <baratwaj3@gmail.com>
2025-06-03 23:21:22 +0530

    insert function

---
+++ String/Some other examples for class string functions.cpp/18.Code using insert function.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string name = "John Doe";
+    name.insert(5, "Michael ");
+    cout << "Full name: " << name << endl;
+    return 0;
+}
