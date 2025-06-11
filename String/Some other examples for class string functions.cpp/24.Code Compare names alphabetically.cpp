53be03f41ae687b158318df17e28921b62eda952
Baratwaj3 <baratwaj3@gmail.com>
2025-06-11 21:56:34 +0530

    alinged

--- String/Some other examples for class string functions.cpp/24.Code Compare names alphabetically.cpp
+++ String/Some other examples for class string functions.cpp/24.Code Compare names alphabetically.cpp
@@ -2,7 +2,8 @@
 #include <string>
 using namespace std;
 
-int main() {
+int main()
+{
     string name1 = "Alice", name2 = "Bob";
     if (name1 < name2)
         cout << name1 << " comes before " << name2 << endl;
