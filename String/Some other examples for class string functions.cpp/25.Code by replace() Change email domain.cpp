2ebb18bfe54febd9f1dfdd90e6d024d4d47106b0
Baratwaj3 <baratwaj3@gmail.com>
2025-06-12 22:55:00 +0530

    replace

---
+++ String/Some other examples for class string functions.cpp/25.Code by replace() Change email domain.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string email = "user@yahoo.com";
+    size_t pos = email.find("yahoo.com");
+    if (pos != string::npos)
+        email.replace(pos, 9, "gmail.com");
+    cout << "Updated email: " << email << endl;
+    return 0;
+}
