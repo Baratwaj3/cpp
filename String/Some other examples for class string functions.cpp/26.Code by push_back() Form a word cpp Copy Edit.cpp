dff4b18164289e63efcf8189608fb62df82ac10c
Baratwaj3 <baratwaj3@gmail.com>
2025-06-14 22:19:07 +0530

    push back function

---
+++ String/Some other examples for class string functions.cpp/26.Code by push_back() Form a word cpp Copy Edit.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string word;
+    word.push_back('C');
+    word.push_back('P');
+    word.push_back('P');
+    cout << "Word: " << word << endl;
+    return 0;
+}
