153636776af4eccefa9f77fc1d83ce2284610f57
Baratwaj3 <baratwaj3@gmail.com>
2025-06-15 12:33:17 +0530

    insert function

---
+++ String/Some other examples for class string functions.cpp/28.insert() Format phone number.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string num = "9876543210";
+    num.insert(5, "-");
+    cout << "Formatted: " << num << endl;
+    return 0;
+}
