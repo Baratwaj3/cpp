f07c5c11f3e63f9105529da08ede2bd039df1f37
Baratwaj3 <baratwaj3@gmail.com>
2025-06-17 18:56:52 +0530

    substr

---
+++ String/Some other examples for class string functions.cpp/29.Code by substr() Extract domain from email.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string email = "name@company.com";
+    size_t at = email.find("@");
+    string domain = email.substr(at + 1);
+    cout << "Domain: " << domain << endl;
+    return 0;
+}
