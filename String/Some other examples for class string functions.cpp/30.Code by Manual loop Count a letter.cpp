167837b5476a542eab1e9e477682bb1541720659
Baratwaj3 <baratwaj3@gmail.com>
2025-06-18 22:39:44 +0530

    manual loop

---
+++ String/Some other examples for class string functions.cpp/30.Code by Manual loop Count a letter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s = "banana";
+    int count = 0;
+    for (char c : s)
+        if (c == 'a') count++;
+    cout << "Number of 'a': " << count << endl;
+    return 0;
+}
