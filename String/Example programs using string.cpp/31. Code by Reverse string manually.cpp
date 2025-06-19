8754a01b3c602e42c1a5f3875c9a46c09332e011
Baratwaj3 <baratwaj3@gmail.com>
2025-06-20 00:01:58 +0530

    reverse a string

---
+++ String/Example programs using string.cpp/31. Code by Reverse string manually.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s = "openai";
+    string rev = "";
+    for (int i = s.size() - 1; i >= 0; --i)
+        rev += s[i];
+    cout << "Reversed: " << rev << endl;
+    return 0;
+}
