0a06b8706e59cd9d5494eb95d55cee75c9d860b4
Baratwaj3 <baratwaj3@gmail.com>
2025-06-27 22:07:47 +0530

    earse functionm

---
+++ String/Some other examples for class string functions.cpp/18.Code by Erase() Remove a word.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string line = "This is a bad idea";
+    size_t pos = line.find("bad ");
+    if (pos != string::npos)
+        line.erase(pos, 4);
+    cout << "Cleaned line: " << line << endl;
+    return 0;
+}
