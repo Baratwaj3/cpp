a5b05859b5c31647c56ce5d62ddd2ad28533030f
Baratwaj3 <baratwaj3@gmail.com>
2025-06-29 23:00:44 +0530

    alinged

--- String/Some other examples for class string functions.cpp/22.Code by using manual loop Replace space with underscore.cpp
+++ String/Some other examples for class string functions.cpp/22.Code by using manual loop Replace space with underscore.cpp
@@ -2,10 +2,13 @@
 #include <string>
 using namespace std;
 
-int main() {
+int main()
+{
     string s = "file name with space.txt";
-    for (char& c : s) {
-        if (c == ' ') c = '_';
+    for (char &c : s)
+    {
+        if (c == ' ')
+            c = '_';
     }
     cout << "Modified: " << s << endl;
     return 0;
