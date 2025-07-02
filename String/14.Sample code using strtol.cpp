4578d260a96cca5e2b5bb98b641b0730f4894aec
Baratwaj3 <baratwaj3@gmail.com>
2025-07-02 23:32:27 +0530

    small changes done

--- String/14.Sample code using strtol.cpp
+++ String/14.Sample code using strtol.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-using namespace std;
 #include <cstring>
+using namespace std;
+
 int main()
 {
     char A[] = "324";
-    int sample_test = strtol(A, NULL, 10);
+    int sample_test = strtol(A, NULL, 10); // Here the 10 is decimal in number system
     cout << "Integer value : " << sample_test << endl;
     sample_test += 1;
     cout << "Integer after added: " << sample_test << endl;
