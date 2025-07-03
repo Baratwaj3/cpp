ce89a7914382fd53439e6fc4ddd94611aefaa280
Baratwaj3 <baratwaj3@gmail.com>
2025-07-04 00:01:39 +0530

    max finction

---
+++ String/22.Sample code using max_size() function.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    size_t M = str.max_size(); 
+    cout << "The maximum size of the string is: " << M << endl;
+    return 0;
+}
