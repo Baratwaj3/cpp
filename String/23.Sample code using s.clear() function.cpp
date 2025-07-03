cf4766ac74d7ddf6b025380488b1821f40bbcc08
Baratwaj3 <baratwaj3@gmail.com>
2025-07-04 00:01:50 +0530

    s.clear function

---
+++ String/23.Sample code using s.clear() function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str="HELLO WORLD";
+    int len_1= str.length();
+    cout << "The length of the string is: " <<len_1 << endl;
+    str.clear();
+    int len_2= str.length();
+    cout << "The length of the string after clear is: " << len_2 << endl;
+    return 0;
+}
