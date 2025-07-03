66c8001f695c3dbbe31de0e0bae78c73d08a0082
Baratwaj3 <baratwaj3@gmail.com>
2025-07-04 00:03:21 +0530

    alinged

--- String/24.Sample code using s.empty() functio.cpp
+++ String/24.Sample code using s.empty() functio.cpp
@@ -5,16 +5,16 @@ using namespace std;
 int main()
 {
     string str;
-    cout<<"Enter a string : ";
-    getline(cin,str);
-    
-    if(str.empty())
+    cout << "Enter a string : ";
+    getline(cin, str);
+
+    if (str.empty())
     {
-        cout<<"The string is empty"<<endl;
+        cout << "The string is empty" << endl;
     }
     else
     {
-        cout<<"The given string is :"<<str<<endl;
+        cout << "The given string is :" << str << endl;
     }
     return 0;
 }
