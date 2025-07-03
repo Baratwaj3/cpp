9bf531677b2e9201090fef462fb087c7a62492f9
Baratwaj3 <baratwaj3@gmail.com>
2025-07-03 23:28:53 +0530

    s.capacity function done

---
+++ String/20.Sample code using s.capacity() function.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string str = "HELLO";
+    int cap = str.capacity();
+    cout << "The capacity of the string is " << cap << endl;
+    return 0;
+}
+/*
+output:
+
+The capacity of the string is 15
+
+*/
