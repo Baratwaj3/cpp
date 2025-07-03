7adb9a671108468d6646badf840ad2de00cb7dae
Baratwaj3 <baratwaj3@gmail.com>
2025-07-04 00:02:38 +0530

    alinged

--- String/22.Sample code using max_size() function.cpp
+++ String/22.Sample code using max_size() function.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     string str;
-    size_t M = str.max_size(); 
+    size_t M = str.max_size();
     cout << "The maximum size of the string is: " << M << endl;
     return 0;
 }
