b22fedd53de909c2a9ee5bbc3701246fafd97291
Baratwaj3 <baratwaj3@gmail.com>
2025-07-04 23:34:05 +0530

    append function

---
+++ String/25.Sample code using s.append() function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str = "HELLO";
+    str.append(" WORLD");
+    cout << "The appended string is : " << str << endl;
+    return 0;
+}
+/*
+
+output:
+
+The appended string is : HELLO WORLD
+
+/*
