52dae61a1c1d2ea72a52cac310a4ba80ac4c1765
Baratwaj3 <baratwaj3@gmail.com>
2025-07-05 00:05:58 +0530

    alinged

--- String/30.Sample code iusing pop_back() function.cpp
+++ String/30.Sample code iusing pop_back() function.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
-int main() {
-  string str="HOW ARE YOUx";
-  str.pop_back();      
-  cout<<"String after pop_back: "<<str<<endl;
-  return 0;
+int main()
+{
+    string str = "HOW ARE YOUx";
+    str.pop_back();
+    cout << "String after pop_back: " << str << endl;
+    return 0;
 }
