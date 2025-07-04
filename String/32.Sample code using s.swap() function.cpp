2837adebfff4347041f67dee2a4392a71d7b0b08
Baratwaj3 <baratwaj3@gmail.com>
2025-07-05 00:08:01 +0530

    alinged

--- String/32.Sample code using s.swap() function.cpp
+++ String/32.Sample code using s.swap() function.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
-#include<string>
+#include <string>
 using namespace std;
-int main() {
-  string str_1=" VIEWER!";
-  string str_2="HOW ARE YOU";
-  str_1.swap(str_2);
-  cout<<str_1<<str_2<<endl;
-  return 0;
+int main()
+{
+    string str_1 = " VIEWER!";
+    string str_2 = "HOW ARE YOU";
+    str_1.swap(str_2);
+    cout << str_1 << str_2 << endl;
+    return 0;
 }
