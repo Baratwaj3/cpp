62402015925b3f997e0d76784e522b88e5ac5eea
Baratwaj3 <baratwaj3@gmail.com>
2025-07-05 00:02:46 +0530

    s.erase function

---
+++ String/29.Sample code using s.erase() function.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  string str = "HOW YOU!";
+  str.erase();
+  cout << "String got erased" << endl;
+  return 0;
+}
+/*
+
+output:
+
+String got erased
+
+*/
