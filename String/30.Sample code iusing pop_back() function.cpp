03799dfb74f2990be41bebb358343dcef49b7568
Baratwaj3 <baratwaj3@gmail.com>
2025-07-05 00:05:44 +0530

    pop back function

---
+++ String/30.Sample code iusing pop_back() function.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+  string str="HOW ARE YOUx";
+  str.pop_back();      
+  cout<<"String after pop_back: "<<str<<endl;
+  return 0;
+}
