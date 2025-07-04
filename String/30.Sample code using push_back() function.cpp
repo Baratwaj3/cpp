7b170039d71f744630a79f5a7d6c49f3085713ef
Baratwaj3 <baratwaj3@gmail.com>
2025-07-05 00:03:54 +0530

    push back function

---
+++ String/30.Sample code using push_back() function.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main() {
+  string str="HOW ARE YOU ";
+  str.push_back('z');      
+  cout<<"String after get added by single char: "<<str<<endl;
+  return 0;
+}
