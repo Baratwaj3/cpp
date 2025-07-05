193e573de195920248874c4e85d8d320b62462af
Baratwaj3 <baratwaj3@gmail.com>
2025-07-06 00:38:15 +0530

    rfind() function

---
+++ String/35.Sample code using s.rfind() function.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    
+       string str_1="welcome";
+       
+       cout<<"LastString finded at the index: "<<str_1.rfind("com")<<endl;
+       
+
+    return 0;
+}
