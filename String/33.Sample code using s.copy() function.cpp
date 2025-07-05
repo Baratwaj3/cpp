5ab2a490ef372b96aacd953ad09dcaa653424a85
Baratwaj3 <baratwaj3@gmail.com>
2025-07-06 00:31:46 +0530

    alinged

--- String/33.Sample code using s.copy() function.cpp
+++ String/33.Sample code using s.copy() function.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
-    
-       string str_1="welcome";
-       char str_2[20];
-       int str_3=str_1.copy(str_2,str_1.length());
-       cout<<"no of char copies: "<<str_3;
+int main()
+{
+
+    string str_1 = "welcome";
+    char str_2[20];
+    int str_3 = str_1.copy(str_2, str_1.length());
+    cout << "no of char copies: " << str_3;
 
     return 0;
 }
