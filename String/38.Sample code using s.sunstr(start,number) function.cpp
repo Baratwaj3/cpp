fe28ef8847bb82115d565dddbb4d26563e311af9
Baratwaj3 <baratwaj3@gmail.com>
2025-07-06 00:46:23 +0530

    substr function

---
+++ String/38.Sample code using s.sunstr(start,number) function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+    string str_1 = "welcome";
+
+    cout << "The substring is: " << str_1.substr(3, 4) << endl;
+
+    return 0;
+}
+/*
+
+output:
+
+The substring is: come
+
+*/
