948a5614478a76a821db1e148b1bcfed14a12d72
Baratwaj3 <baratwaj3@gmail.com>
2025-07-06 00:43:37 +0530

    find_last_of function

---
+++ String/37.Sample code using s.find_last_of() function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+    string str_1 = "welcome";
+
+    cout << "Last char finded at the index: " << str_1.find_last_of('e') << endl;
+
+    return 0;
+}
+/*
+
+output:
+
+First char finded at the index: 6
+
+*/
