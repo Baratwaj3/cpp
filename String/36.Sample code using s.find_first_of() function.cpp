a7807a8451d56d1c81a703ddb42314392ce74253
Baratwaj3 <baratwaj3@gmail.com>
2025-07-06 00:41:11 +0530

    alinged

--- String/36.Sample code using s.find_first_of() function.cpp
+++ String/36.Sample code using s.find_first_of() function.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
-    
-       string str_1="welcome";
-       
-       cout<<"First char finded at the index: "<<str_1.find_first_of('c')<<endl;
-       
+int main()
+{
+
+    string str_1 = "welcome";
+
+    cout << "First char finded at the index: " << str_1.find_first_of('c') << endl;
 
     return 0;
 }
