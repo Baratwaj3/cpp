b80faf785c974036d34581479e79aa92d1cce5f6
Baratwaj3 <baratwaj3@gmail.com>
2025-07-07 00:32:40 +0530

    iterator method

---
+++ String/40.Sample code for accessing element in a string using iterator method.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str="TODAY";
+   string::iterator it;
+   for(it=str.begin();it!=str.end();it++)
+   {
+       cout<<*it;
+   }
+    return 0;
+}
