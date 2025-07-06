05277a7d998b4db2bc92de6df22bdb7f5a8b5e6a
Baratwaj3 <baratwaj3@gmail.com>
2025-07-07 00:44:40 +0530

    alinged

--- String/42.Sample code for accessing element in a string using for loop.cpp
+++ String/42.Sample code for accessing element in a string using for loop.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int main() {
-    string str="TODAY";
-   for(int i=0;str[i]!='\0';i++)
-   {
-       
-       cout<<str[i];
-   }
+int main()
+{
+    string str = "TODAY";
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+
+        cout << str[i];
+    }
     return 0;
 }
