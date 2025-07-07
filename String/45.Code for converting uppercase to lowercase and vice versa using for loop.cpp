b3ee524318764b968f51f312644891e4508c0527
Baratwaj3 <baratwaj3@gmail.com>
2025-07-07 22:57:27 +0530

    alinged

--- String/45.Code for converting uppercase to lowercase and vice versa using for loop.cpp
+++ String/45.Code for converting uppercase to lowercase and vice versa using for loop.cpp
@@ -7,22 +7,18 @@ int main()
     cout << "Enter the string" << endl;
     getline(cin, str);
 
-  
     for (int i = 0; str[i] != '\0'; i++)
     {
-      if(str[i]>='A'&&str[i]<='Z')
-      {
-          str[i]+=32;
-      }
-      else if (str[i]>='a'&&str[i]<='z')
-      {
-          str[i]-=32;
-      }
-      cout<<str[i];
+        if (str[i] >= 'A' && str[i] <= 'Z')
+        {
+            str[i] += 32;
+        }
+        else if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] -= 32;
+        }
+        cout << str[i];
     }
-    
-   
+
     return 0;
 }
-
-
