1f7a21232cf90b55d2863b4f2e3ce8a53a0f09d3
Baratwaj3 <baratwaj3@gmail.com>
2025-07-07 22:36:15 +0530

    output added

--- String/44.Code for finding length of a string using iterator method.cpp
+++ String/44.Code for finding length of a string using iterator method.cpp
@@ -18,3 +18,23 @@ int main()
 
     return 0;
 }
+/*
+output:
+Enter the string
+Welcome viewer
+character1=W
+character2=e
+character3=l
+character4=c
+character5=o
+character6=m
+character7=e
+character8= 
+character9=v
+character10=i
+character11=e
+character12=w
+character13=e
+character14=r
+Total characters: 14
+*/
