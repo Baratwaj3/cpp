f46bf642a4fbcec3ae74d31fdfc57088551e0673
Baratwaj3 <baratwaj3@gmail.com>
2025-07-07 22:57:10 +0530

    upper to lower and vive

---
+++ String/45.Code for converting uppercase to lowercase and vice versa using for loop.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string str;
+    cout << "Enter the string" << endl;
+    getline(cin, str);
+
+  
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+      if(str[i]>='A'&&str[i]<='Z')
+      {
+          str[i]+=32;
+      }
+      else if (str[i]>='a'&&str[i]<='z')
+      {
+          str[i]-=32;
+      }
+      cout<<str[i];
+    }
+    
+   
+    return 0;
+}
+
+
