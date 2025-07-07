86d5920edcd0b12c9095c77b9cf9f44faab0383f
Baratwaj3 <baratwaj3@gmail.com>
2025-07-07 22:28:53 +0530

    length of a string

---
+++ String/43.Code for finding length of a string.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str;
+    cout<<"Enter the string"<<endl;
+    getline(cin,str);
+    
+    int count=0;
+    for(int i=0;str[i]!='\0';i++)
+    {
+        count++;
+        cout<<"character"<<i+1<<"="<<str[i]<<endl;
+        
+    }
+    cout<<"Total characters: "<<count<<endl;
+
+    return 0;
+}
