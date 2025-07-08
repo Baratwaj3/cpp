f1f7f2097126a902be0744ae03c9d2b3cb0796e2
Baratwaj3 <baratwaj3@gmail.com>
2025-07-09 01:09:58 +0530

    username from the email

---
+++ String/47.Code for finding username from a email.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string email;
+    cout<<"Enter your E-mail: ";
+    cin>>email;
+    int i = email.find('@');
+    string j = email.substr(0, i);
+    int len = j.length();
+    bool valid = true; 
+
+    for (int k = 0; k < len; k++) {
+        char ch = j[k];
+        if (!((ch >= 'a' && ch <= 'z') ||
+              (ch >= 'A' && ch <= 'Z') ||
+              (ch >= '0' && ch <= '9') ||
+              (ch == '_'))) {
+            valid = false;
+            break;
+        }
+    }
+
+    if (valid == true)
+        cout << "Valid username: " << j << endl;
+    else
+        cout << "Invalid username: " << j << endl;
+
+    return 0;
+}
