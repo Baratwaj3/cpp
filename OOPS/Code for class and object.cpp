861693e34bdfe8fab782f04b1103c348c3c8cf8d
Baratwaj3 <baratwaj3@gmail.com>
2025-07-09 23:41:16 +0530

    undo

---
+++ OOPS/Code for class and object.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Car
+{
+public:
+    void display()
+    {
+        cout << "This is a car.\n";
+    }
+};
+
+int main()
+{
+    Car c;
+    c.display();
+    return 0;
+}
