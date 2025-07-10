46d3bae94508eadab5cfd21ef9e1cf468bf6d7f7
Baratwaj3 <baratwaj3@gmail.com>
2025-07-11 00:45:24 +0530

    spaced for readability

--- OOPS/3.Code for finding area and perimeter of a rectangle using pointer in heap.cpp
+++ OOPS/3.Code for finding area and perimeter of a rectangle using pointer in heap.cpp
@@ -5,6 +5,7 @@ class rectangle
 public:
     int length;
     int breath;
+
     int area()
     {
         return length * breath;
@@ -18,8 +19,10 @@ public:
 int main()
 {
     rectangle *p = new rectangle; // used to allocate memory in heap
+
     p->length = 10;
     p->breath = 20;
+    
     cout << "Area of the rectangle: " << p->area() << endl;
     cout << "Perimater of the rectangle: " << p->perimeter() << endl;
 
