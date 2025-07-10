521161cdca426e7fe48d14a2bd5a978fa818a2c5
Baratwaj3 <baratwaj3@gmail.com>
2025-07-11 00:37:12 +0530

    alinged

--- OOPS/2.Code for finding area and perimeter of a rectangle using pointer in stack.cpp
+++ OOPS/2.Code for finding area and perimeter of a rectangle using pointer in stack.cpp
@@ -2,28 +2,28 @@
 using namespace std;
 class rectangle
 {
-    public:
-    
+public:
     int length;
     int breath;
     int area()
     {
-        return length*breath;
+        return length * breath;
     }
-   int  perimeter()
+    int perimeter()
     {
-        return 2*(length+breath);
+        return 2 * (length + breath);
     }
 };
 
-int main() {
-   rectangle r;
-   rectangle *p;
-   p=&r;
-   p->length=10;
-   p->breath=20;
-   cout<<"Area of the rectangle: "<<p->area()<<endl;
-   cout<<"Perimater of the rectangle: "<<p->perimeter()<<endl;
-   
+int main()
+{
+    rectangle r;
+    rectangle *p;
+    p = &r;
+    p->length = 10;
+    p->breath = 20;
+    cout << "Area of the rectangle: " << p->area() << endl;
+    cout << "Perimater of the rectangle: " << p->perimeter() << endl;
+
     return 0;
 }
