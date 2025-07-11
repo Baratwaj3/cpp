711638d72920c523208ebe0378ab81ef7be4ac9c
Baratwaj3 <baratwaj3@gmail.com>
2025-07-12 00:10:49 +0530

    accessor and mutator

---
+++ OOPS/5.Code for finding area and perimeter of a rectangle using get(accessor) and set (mutator) functions.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle {
+private:
+    int length;
+    int breadth;
+
+public:
+    // Setter for length
+    void setlength(int l) {
+        if (l >= 0)
+            length = l;
+        else
+            cout << "Not a valid length" << endl;
+    }
+
+    // Setter for breadth
+    void setbreadth(int b) {
+        if (b >= 0)
+            breadth = b;
+        else
+            cout << "Not a valid breadth" << endl;
+    }
+    int getlength(){
+        return length;
+    }
+     int getbreadth() {
+        return breadth;
+    }
+
+    // Area function
+    int area() {
+        return length * breadth;
+    }
+
+    // Perimeter function
+    int perimeter() {
+        return 2 * (length + breadth);
+    }
+};
+
+int main() {
+    Rectangle r;
+    r.setlength(10);
+    r.setbreadth(20);
+    cout<<"Length: "<<r.getlength()<<endl;
+    cout<<"Breadth: "<<r.getbreadth()<<endl;
+    cout << "Area: " << r.area() << endl;
+    cout << "Perimeter: " << r.perimeter() << endl;
+
+    return 0;
+}
