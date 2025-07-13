d788f5718245dc02470c8ef12b9ef3b3a9d6c522
Baratwaj3 <baratwaj3@gmail.com>
2025-07-13 22:45:10 +0530

    added

---
+++ OOPS/Example codes/4.Multilevel Inheritance.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    void sound() {
+        cout << "Animal Sound\n";
+    }
+};
+
+class Dog : public Animal {
+public:
+    void bark() {
+        cout << "Dog Barking\n";
+    }
+};
+
+class Puppy : public Dog {
+public:
+    void weep() {
+        cout << "Puppy Weeping\n";
+    }
+};
+
+int main() {
+    Puppy p;
+    p.sound();
+    p.bark();
+    p.weep();
+    return 0;
+}
