d8594fe2317f8254b58f7190a2085e5d48ebc454
Baratwaj3 <baratwaj3@gmail.com>
2025-07-16 22:28:40 +0530

    non parametrized constructor

---
+++ OOPS/6.Code for rectangle using Non-Parameterized constructor.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+private:
+    int length;
+    int breadth;
+
+public:
+    // Non-parameterized constructor
+    Rectangle()
+    {
+        length = 0;
+        breadth = 0;
+    }
+
+    void setlength(int l)
+    {
+        if (l >= 0)
+            length = l;
+        else
+            cout << "Not a valid length" << endl;
+    }
+
+    void setbreadth(int b)
+    {
+        if (b >= 0)
+            breadth = b;
+        else
+            cout << "Not a valid breadth" << endl;
+    }
+
+    int getlength() { return length; }
+    int getbreadth() { return breadth; }
+
+    int area() { return length * breadth; }
+    int perimeter() { return 2 * (length + breadth); }
+};
+
+int main()
+{
+    Rectangle r; // non-parameterized constructor
+    r.setlength(8);
+    r.setbreadth(4);
+
+    cout << "Length: " << r.getlength() << endl;
+    cout << "Breadth: " << r.getbreadth() << endl;
+    cout << "Area: " << r.area() << endl;
+    cout << "Perimeter: " << r.perimeter() << endl;
+
+    return 0;
+}
